a8afa20dd28833ce74eef5f14283afe2bbf583da
Sidney Pham <sidneypham1@gmail.com>
2021-10-10 15:19:06 +1100

    Up to Exercise 3.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
+      {heroPicture}
+      <Swoop alt="" src="/swoop.svg" />
     </Wrapper>
   );
 };
@@ -26,6 +26,31 @@ const HeroImage = styled.img`
   height: 500px;
   max-height: 100%;
 `;
+
+const heroPicture = (
+  <picture>
+    <source
+      type="image/avif"
+      srcset="
+        /images/hero-img.avif 1x,
+        /images/hero-img@2x.avif 2x,
+        /images/hero-img@3x.avif 3x
+      "
+    />
+    <source
+      type="image/jpeg"
+      srcset="
+        /images/hero-img.jpg 1x,
+        /images/hero-img@2x.jpg 2x,
+        /images/hero-img@3x.jpg 3x
+      "
+    />
+    <HeroImage
+      alt="Serious looking cat with blue eyes in front of dark background"
+      src="/images/hero-img.jpg"
+    />
+  </picture>
+);
 
 const Swoop = styled.img`
   position: absolute;
